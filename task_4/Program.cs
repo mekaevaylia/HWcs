56afe3e6aaf164aa69dd5c100ee62c2b27dd20ae
iiuliia <mekaeva.ylia@yandex.ru>
2023-07-23 02:34:08 +0500

    задание 2

---
+++ task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int num = 1;
+while (num <= a)
+{
+    if (num % 2 == 0)
+    {
+        Console.Write($"{num} ");
+
+    }
+    num++;
+}
+
