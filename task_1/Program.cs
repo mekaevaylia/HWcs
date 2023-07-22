7cf913c181e2952c66815c0f4be0f040646624f3
iiuliia <mekaeva.ylia@yandex.ru>
2023-07-23 02:31:01 +0500

    Задание 1

---
+++ task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+if(a > b) {
+    Console.WriteLine($"Число {a} = max");
+}
+else Console.WriteLine($"Число {b} = max");
+
