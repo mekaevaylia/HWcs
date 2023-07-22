3a70e6e7872091eeb828a145dc55579a233943a0
iiuliia <mekaeva.ylia@yandex.ru>
2023-07-23 02:53:11 +0500

    Задание 3

---
+++ task_3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if(a % 2 == 0) {
+    Console.WriteLine($"{a} - четное число");
+}
+else Console.WriteLine($"{a} - нечетное число");
+
