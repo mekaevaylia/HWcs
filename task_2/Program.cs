34a438f591cb805c5bbd373a93e0968b4b5350a8
iiuliia <mekaeva.ylia@yandex.ru>
2023-07-23 02:49:35 +0500

    Задание 2

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if(b > a) {
+   max = b;
+};
+if(c > max) {
+    max = c;
+};
+Console.WriteLine($"Maксимальное - {max}");
